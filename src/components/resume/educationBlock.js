2bb892eb1c046f19f17f7098676ac0c02a23dab0
pauljay611 <pauljay611@gmail.com>
2020-01-21 22:11:30 +0800

    fix typo

--- src/components/resume/educationBlock.js
+++ src/components/resume/educationBlock.js
@@ -2,18 +2,16 @@ import React from "react"
 import { InfoContent, TimeBlock } from "./common"
 
 const EducationBlock = () => (
-    <div>
-        <h2>
-            Education
-                </h2>
-        <InfoContent>
-            <h4>National Chiai University</h4>
-            <TimeBlock>
-                <p>Bachelor, Mangement Information System</p>
-                <p>Graduated -  September 2017</p>
-            </TimeBlock>
-        </InfoContent>
-    </div>
+  <div>
+    <h2>Education</h2>
+    <InfoContent>
+      <h4>National Chiayi University</h4>
+      <TimeBlock>
+        <p>Bachelor, Mangement Information System</p>
+        <p>Graduated - September 2017</p>
+      </TimeBlock>
+    </InfoContent>
+  </div>
 )
 
 export default EducationBlock
